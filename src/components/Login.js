0e319585a90e949c0aab9e61da470338f1a31c40
Mradul Singhal <singhalmradul@gmail.com>
2023-03-19 12:27:18 -0400

    optimize css

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,17 +4,19 @@ class Login extends Component {
 	render() {
 		return (
 			<div className='login'>
-				<h1>Login</h1>
+				<h1>login</h1>
 				<form>
-					<label>username</label>
+					<label className='label'>username</label>
 					<input
+						className='field'
 						type='text'
 						placeholder='username'
 						required
 					/>
 					<br />
-					<label>password</label>
+					<label className='label'>password</label>
 					<input
+						className='field'
 						type='password'
 						placeholder='password'
 						required
